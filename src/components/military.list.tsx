f26c597eecbdd52c48487d4bd23ad72f1bc04412
Gadelha TI <gadelha.ti@gmail.com>
2022-10-28 16:32:32 -0300

    cleaning up

--- src/components/military.list.tsx
+++ src/components/military.list.tsx
@@ -6,7 +6,6 @@ import list from "./list.json"
 export const MilitaryList = () => {
     const [scale, setScale] = useState<string[]>([])
     const [militaryList, setMilitaryList] = useState<Military[]>(list)
-    // const [militaryCurrent, setMilitaryCurrent] = useState<Military>(militaryInitial)
 
     const couting = (activation: boolean): number => {
         let vector: number = 0
@@ -40,16 +39,9 @@ export const MilitaryList = () => {
         addList(service1)
         addList(service2)
         console.log(militaryList)
-        // setMilitaryCurrent(service)
     }
     const addList = (military: Military) => {
         setMilitaryList([...militaryList.filter(item => item != military), military])
-    }
-    const julianDay = (): any => {
-        var d = new Date().getDate();
-        var y = new Date().getFullYear();
-        var m = new Date().getMonth() + 1;
-        return Math.floor((1461 * (y + 4800 + (m - 14) / 12)) / 4 + (367 * (m - 2 - 12 * ((m - 14) / 12))) / 12 - (3 * ((y + 4900 + (m - 14) / 12) / 100)) / 4 + d - 32075);
     }
     return (
         <>
