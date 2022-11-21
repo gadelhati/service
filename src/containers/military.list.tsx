b05dbadc4d8b8a5083b135c4749248c09b6bcab0
Gadelha TI <gadelha.ti@gmail.com>
2022-11-21 14:18:47 -0300

    cleaning up

--- src/containers/military.list.tsx
+++ src/containers/military.list.tsx
@@ -2,14 +2,11 @@ import { useState } from "react"
 import { militaryInitial } from "../components/military/military.initial"
 import { Military } from "../components/military/military.interface"
 import list from "../components/military/list.json"
-import { Division } from "../components/division/division.interface"
-import { divisionInitial } from "../components/division/division.initial"
 
 export const MilitaryList = () => {
     const [scale, setScale] = useState<string[]>(["D","S","T","Q","Q","S","S","D","S","T","Q","Q","S","S","D","S","T","Q","Q","S","S","D","S","T","Q","Q","S","S","D","S","T","Q","Q","S","S","T","Q","Q","S","S","D","S","T","Q","Q","S","S","D","S","T","Q","Q","S","S","D","S","T","Q","Q","S","S","D","S","T","Q","Q","S","S"])
     const [militaryList, setMilitaryList] = useState<Military[]>(list)
-    const [division, setDivision] = useState<Division[]>([divisionInitial, divisionInitial, divisionInitial, divisionInitial, divisionInitial])
-    const [ms, setMs] = useState<Military[][]>([])
+    const [division, setDivision] = useState<Military[][]>([])
 
     const quickSort = (array: Military[]): Military[] => {
         if (array.length <= 1) {
@@ -32,49 +29,29 @@ export const MilitaryList = () => {
     const composeDivision = () => {
         let total: number = Math.floor(militaryList.length/5)
         let rest: number = militaryList.length % 5
-        let serviceDivision: Division[] = [divisionInitial]
         let i: number = 0
         let aux: number = 0
-        // console.log(total + " - " + rest + " - " + i + " - " + militaryList.length)
-        for (i = 0; i <= militaryList.length + 1; i = i + total + rest) {
-            // if(i + total + rest < militaryList.length){
+        for (i = 0; i < militaryList.length; i = i + total + rest) {
                 aux = i + total
                 if(rest > 0) {
-                    console.log(i + " - "+ aux + " - "+ rest)
-                    console.log(militaryList.slice(i, aux + 1))
-                    ms.push(militaryList.slice(i, aux + 1))
-                    // setMs([militaryList.slice(i, aux + 1)])
-                    // ms[i] = militaryList.slice(i, aux + 1)
+                    division.push(militaryList.slice(i, aux + 1))
                     rest--
                     if(rest == 0){i++}
                 }
-                // if(rest == 0) { i++ }
-                // if(rest <= 0) {
                 else {
-                    // i++
-                    console.log(i + " - "+ aux)
-                    console.log(militaryList.slice(i, aux))
-                    ms.push(militaryList.slice(i, aux))
-                    // ms.push(militaryList.slice(i, i + total))
-                    // setMs([militaryList.slice(i, i + total)])
-                    // ms[i] = militaryList.slice(i, i + total)
+                    division.push(militaryList.slice(i, aux))
                 }
-                // if(rest > 0){ rest -- }
-            // }
         }
-        // console.log(total + " - " + rest + " - " + i + " - " + militaryList.length)
-        // console.log(ms)
     }
     const showDivision = () => {
         console.log(division)
     }
-
-
     const couting = (activation: boolean): number => {
         let vector: number = 0
         militaryList?.map(element => { if (element.active == activation) return vector += 1 })
         return vector
     }
+    
     const nextService = (militaries: Military[], date: Date): Military => {
         let militarOfService: Military = militaryInitial
         let datt: Date = new Date()
@@ -126,7 +103,7 @@ export const MilitaryList = () => {
         <>
             <button onClick={sort} >1 - Sort List</button>
             <button onClick={showList} >2 - Show List</button>
-            <button onClick={composeDivision} >{militaryList.length} - Compose Division</button>
+            <button onClick={composeDivision} >{couting(true)}/{militaryList.length} - Compose Division</button>
             <button onClick={showDivision} >Show Division</button>
             <button onClick={semanal} >Escalar</button>
         </>)
